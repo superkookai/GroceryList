7a5e13492cd0b5e3511dd47f2879792d9a411203
Weerawut <weerawut@live.com>
2025-02-22 13:44:13 +0700

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Weerawut Chaiyasomboon on 22/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
